181e14457c1e945648150c9912c0c3a4d86ce16a
SpicyOwl <duanxu@bc.edu>
2018-11-28 23:37:53 -0500

    Add files via upload

--- pythonTimeTesting/QuickFind.py
+++ pythonTimeTesting/QuickFind.py
@@ -12,9 +12,12 @@ def connected_QF(initial_id, p,q):#return if p and q are connected
 
 def union_QF(initial_id, p, q):#union the thing
     local_list = initial_id
-    p_id = local_list[p]
-    q_id = local_list[q]
-    for i in range(0,len(initial_id)):
-        if(local_list[i] == p_id):
-            local_list[i] = q_id
+    p_id = local_list[p]#p's id
+    q_id = local_list[q]#q's id
+
+    if p_id != q_id:#only go through for loop when
+                    #they are not in the same component
+        for i in range(0,len(initial_id)):
+            if(local_list[i] == p_id):
+                local_list[i] = q_id
     return local_list
--- pythonTimeTesting/improvements.py
+++ pythonTimeTesting/improvements.py
@@ -1,3 +1,5 @@
+import QuickFind
+import QuickUnion
 #improvement1
 #weighted quick_union
 
@@ -11,27 +13,29 @@
 def Weighted_QU_List_Initial(n):
     count = n
     initial_list = []
+    #python will increse size when it needs
+    #thus decrease running time
     for i in range(0, n):
         initial_list.append(i)
     return initial_list
-def Weighted_QU_Size_Initial(initial_list):
-    size = []
+def Weighted_QU_Sz_Initial(initial_list):
+    sz = []
     for i in range(0,len(initial_list)):
-        size.append(1)
-    return size
-
+        sz.append(1)
+    return sz
 
 def weighted_union_QU(initial_id,size_list, p, q):
 #the initial_id will be modified after completion
     local_list = initial_id
-    i = find_root_QU(initial_id,p)
-    j = find_root_QU(initial_id,q)
-    if(size_list[i]<size_list[j]):
-        local_list[i] = j
-        size_list[j] = size_list[i]+size_list[j]
-    else:
-        local_list[j] = i
-        size_list[i] = size_list[i]+size_list[j]
+    i = QuickUnion.find_root_QU(initial_id,p)
+    j = QuickUnion.find_root_QU(initial_id,q)
+    if(i != j):
+        if(size_list[i]<size_list[j]):
+            local_list[i] = j
+            size_list[j] = size_list[i]+size_list[j]
+        else:
+            local_list[j] = i
+            size_list[i] = size_list[i]+size_list[j]
 
     return local_list
 
@@ -43,3 +47,18 @@ def find_root_PC(initial_id, i):#find the root of node i
         initial_id[i]=initial_id[initial_id[i]]
         i = initial_id[i]
     return i #return the root
+
+def PathCompressionUnion(initial_id,size_list, p, q):
+#the initial_id will be modified after completion
+    local_list = initial_id
+    i = find_root_PC(initial_id,p)#PC way to find root
+    j = find_root_PC(initial_id,q)
+    if(i != j):
+        if(size_list[i]<size_list[j]):
+            local_list[i] = j
+            size_list[j] = size_list[i]+size_list[j]
+        else:
+            local_list[j] = i
+            size_list[i] = size_list[i]+size_list[j]
+
+    return local_list
